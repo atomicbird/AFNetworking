5db68ee6fcba1b053c6fafca848e780dfddc6556
Tom Harrington <tph@atomicbird.com>
2011-10-05 10:50:31 -0600

    Remove JSONKit (and therefore, support for iOS versions before iOS 5)

--- AFNetworking/AFJSONRequestOperation.m
+++ AFNetworking/AFJSONRequestOperation.m
@@ -21,7 +21,6 @@
 // THE SOFTWARE.
 
 #import "AFJSONRequestOperation.h"
-#import "JSONKit.h"
 
 #include <Availability.h>
 
@@ -99,13 +98,9 @@ static dispatch_queue_t json_request_operation_processing_queue() {
                 id JSON = nil;
                 NSError *JSONError = nil;
 #if __IPHONE_OS_VERSION_MIN_REQUIRED > __IPHONE_4_3
-                if ([NSJSONSerialization class]) {
-                    JSON = [NSJSONSerialization JSONObjectWithData:data options:0 error:&JSONError];
-                } else {
-                    JSON = [[JSONDecoder decoder] objectWithData:data error:&JSONError];
-                }
+                JSON = [NSJSONSerialization JSONObjectWithData:data options:0 error:&JSONError];
 #else
-                JSON = [[JSONDecoder decoder] objectWithData:data error:&JSONError];
+#warning This version of AFNetworking requires iOS 5 or higher.
 #endif
                 
                 dispatch_async(dispatch_get_main_queue(), ^(void) {
